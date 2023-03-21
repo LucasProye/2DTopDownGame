e972d8d8ba9e7c650eaa02c8bba844b066094a4b
Lucas <lucas.eartsup@gmail.com>
2023-03-21 11:57:01 +0100

    aaa

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,15 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     PlayerInput playerInput;
-
     Rigidbody2D playerRigidbody2D;
-
     Vector2 direction;
-
     [SerializeField] float speed = 5f;
+    [SerializeField] Text _scoring;
+    int _score = 0;
 
     void Start()
     {
@@ -31,5 +31,14 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         playerRigidbody2D.velocity = direction * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Coins"))
+        {
+            _score += 1;
+            _scoring.text = "" + _score;
+        }
     }
 }
--- Assets/Scripts/Scoring/Coins.cs
+++ Assets/Scripts/Scoring/Coins.cs
@@ -5,15 +5,10 @@ using UnityEngine.UI;
 
 public class Coins : MonoBehaviour
 {
-    [SerializeField] Text _scoring;
-    int _score = 0;
-
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player"))
         {
-            _score += 1;
-            _scoring.text = "" + _score;
             Destroy(gameObject);
         }
     }
