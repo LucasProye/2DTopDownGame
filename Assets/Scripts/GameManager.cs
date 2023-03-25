5edbfc226e114a605ccce8b0b137fec7584af0ec
Lucas <94801792+LucasProye@users.noreply.github.com>
2023-03-25 17:17:24 +0100

    GameManager + Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,30 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private TimerGame _timerGame;
-    [SerializeField] private GameObject _level1;
-    [SerializeField] private GameObject _level2;
-    [SerializeField] private GameObject _level3;
+
+    [SerializeField] private GameObject[] _level;
+
+    Vector3 _placementLevel = new Vector3(0, 0, 0);
+    public bool _level1IsPlay = false;
+    public bool _level2IsPlay = false;
+    public bool _level3IsPlay = false;
+    public bool _level4IsPlay = false;
 
     private void Start()
     {
-        Instantiate(_level1, transform.position, quaternion.identity);
+        Instantiate(_level[0], _placementLevel, quaternion.identity);
+        _level1IsPlay = true;
     }
 
-    private void Update()
+    private void LevelUpdate()
     {
-        
+        if(_timerGame._timeLeft == 0 && _level1IsPlay == true)
+        {
+            _level1IsPlay = false;
+            Debug.Log("level 1 is false");
+            Instantiate(_level[1], _placementLevel, quaternion.identity);
+            _level2IsPlay = true;
+            Debug.Log("level 2 is true");
+        }
     }
-
-
 }
--- Assets/Scripts/Timer/TimerGame.cs
+++ Assets/Scripts/Timer/TimerGame.cs
@@ -7,5 +7,50 @@ using UnityEngine.UI;
 
 public class TimerGame : MonoBehaviour
 {
-    
+    private GameManager _gm;
+    public float _timeLeft;
+    [SerializeField] private bool _timerOn = false;
+    public Text _timerTxt;
+
+    void Start()
+    {
+        _timerOn = true;
+    }
+
+    void Update()
+    {
+        if (_timerOn)
+        {
+            if (_timeLeft > 0)
+            {
+                _timeLeft -= Time.deltaTime;
+                updateTimer(_timeLeft);
+            }
+            else
+            {
+                _timeLeft = 0;
+                _timerOn = false;
+            }
+        }
+
+        if(_gm._level1IsPlay == true)
+        {
+            _timeLeft = 5;
+        }
+
+        if (_gm._level2IsPlay == true)
+        {
+            _timeLeft = 10;
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        _timerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
