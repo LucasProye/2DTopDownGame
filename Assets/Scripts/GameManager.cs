1b842538512030c533620871f0453a4a9df210ba
Lucas <lucas.eartsup@gmail.com>
2023-03-27 13:09:07 +0200

    aaa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     private TimerGame _timerGame;
-
     [SerializeField] private GameObject[] _level;
-
     Vector3 _placementLevel = new Vector3(0, 0, 0);
     public bool _level1IsPlay = false;
     public bool _level2IsPlay = false;
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour
         {
             _level2IsPlay = true;
             Instantiate(_level[1], _placementLevel, quaternion.identity);
-            Debug.Log("level 2 is true");   
+            Debug.Log("level 2 is true");
         }
     }
 }
